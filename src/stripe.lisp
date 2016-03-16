ded9bafb5b38b887e06b8dd68ec52820b8bd251c
Ilya Khaprov <dead.trickster@gmail.com>
2016-03-16 15:21:09 -0700

    make-stripe can accept ssl and retries options

--- src/stripe.lisp
+++ src/stripe.lisp
@@ -4,16 +4,17 @@
 
 (defclass stripe ()
   ((secret-key :initarg :secret-key :reader stripe-secret-key)
-   (max-retries :initarg :max-retries :initform 0)
-   (max-retry-delay :initarg :max-retry-delay :initform 10)
-   (retry-delay :initarg :retry-delay :initform 2)
+   (max-retries :initarg :max-retries)
+   (max-retry-delay :initarg :max-retry-delay)
+   (retry-delay :initarg :retry-delay)
    (ssl-ctx :initarg :ssl-ctx :reader stripe-ssl-ctx) ;; TODO: or maybe use one global context
    ))
 
-(defun make-stripe-ssl-ctx ()
-  (cl+ssl:make-context))
-
-(defun make-stripe (secret-key)
-  (let* ((ssl-ctx (make-stripe-ssl-ctx))
-         (stripe (make-instance 'stripe :secret-key secret-key :ssl-ctx ssl-ctx)))
+(defun make-stripe (secret-key &key (ssl-options '(:verify-location :default)) (max-retries 0) (max-retry-delay 10) (retry-delay 2))
+  (let* ((ssl-ctx (apply #'cl+ssl:make-context ssl-options))
+         (stripe (make-instance 'stripe :secret-key secret-key
+                                        :ssl-ctx ssl-ctx
+                                        :max-retries max-retries
+                                        :max-retry-delay max-retry-delay
+                                        :retry-delay retry-delay)))
     (trivial-garbage:finalize stripe (lambda () (cl+ssl:ssl-ctx-free ssl-ctx)))))
